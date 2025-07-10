8a3c8f3f69adc6c80679459dcac2510c0b2fd8e5
devreposhp <inteelbr@hotmail.com>
2025-07-10 00:22:23 -0300

    Primeiro

---
+++ re1.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  <title>RE MODEL</title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
+<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.0.7/css/swiper.min.css'>
+  <link rel="stylesheet" href="https://wakegg.github.io/assets/css/style1.css?v=313">
+
+</head>
+<body>
+<div class="swiper-container main-slider loading">
+  <div class="swiper-wrapper">
+
+    <div class="swiper-slide">
+      <figure class="slide-bgimg" style="background-image:url(https://i.imgur.com/oedGfeJ.png);">
+        <img src="" class="entity-img" />
+      </figure>
+      <div class="content">
+        <p class="title">Wake</p>
+        <span class="caption">Normal</span>
+      </div>
+    </div>
+
+    <div class="swiper-slide">
+      <figure class="slide-bgimg" style="background-image:url(https://i.imgur.com/BkGx3N0.png);">
+        <img src="https://i.imgur.com/BkGx3N0.png" class="entity-img" />
+      </figure>
+      <div class="content">
+        <p class="title">Wake</p>
+        <span class="caption">Normal</span>
+      </div>
+    </div>
+
+    <div class="swiper-slide">
+      <figure class="slide-bgimg" style="background-image:url(https://i.imgur.com/T6S7tgN.png);">
+        <img src="" class="entity-img" />
+      </figure>
+      <div class="content">
+        <p class="title">Wake</p>
+        <span class="caption">Bonus</span>
+      </div>
+    </div>
+
+    <div class="swiper-slide">
+      <figure class="slide-bgimg" style="background-image:url(https://i.imgur.com/T6S7tgN.png);">
+        <img src="" class="entity-img" />
+      </figure>
+      <div class="content">
+        <p class="title">Wake</p>
+        <span class="caption">Bonus</span>
+      </div>
+    </div>
+
+    <div class="swiper-slide">
+      <figure class="slide-bgimg" style="background-image:url(https://i.imgur.com/T6S7tgN.png);">
+        <img src="" class="entity-img" />
+      </figure>
+      <div class="content">
+        <p class="title">Wake</p>
+        <span class="caption">Bonus</span>
+      </div>
+    </div>
+    <style>
+      .swiper-button-prev {
+       height: 15% !important; 
+      }
+      .swiper-button-next {
+       height: 15% !important; 
+      }
+      </style>
+    
+  </div>
+  <div class="swiper-button-prev swiper-button-white" style="padding: 20px; border-radius: 4px; background-color: rgba(0, 0, 0, 0.486);">
+    <img src="https://i.imgur.com/GZ4IQ8z.png" style="position: absolute;height: 50%;margin-top: 4px;" class="" /></div>
+  <div class="swiper-button-next swiper-button-white" style="padding: 20px; border-radius: 4px; background-color: rgba(0, 0, 0, 0.486);">
+    <img src="https://i.imgur.com/DxrNNqs.png" style="position: absolute;height: 50%;margin-top: 4px;margin-left: -38px;" class="" /></div>
+</div>
+
+<div class="swiper-container nav-slider loading">
+  <div class="swiper-wrapper" role="navigation">
+
+    <div class="swiper-slide">
+      <figure class="slide-bgimga" style="background-image:url(https://i.imgur.com/oedGfeJ.png)">
+        <img src="https://i.imgur.com/oedGfeJ.png" class="entity-img" />
+      </figure>
+      <div class="content" style="background-color:transparent;">
+        <p class="title"> </p>
+      </div>
+    </div>
+
+    <div class="swiper-slide">
+      <figure class="slide-bgimga" style="background-image:url(https://i.imgur.com/BkGx3N0.png)">
+        <img src="https://i.imgur.com/BkGx3N0.png" class="entity-img" />
+      </figure>
+      <div class="content" style="background-color:transparent;">
+        <p class="title"> </p>
+      </div>
+    </div>
+
+    <div class="swiper-slide">
+      <figure class="slide-bgimga" style="background-image:url(https://i.imgur.com/T6S7tgN.png)">
+        <img src="https://i.imgur.com/T6S7tgN.png" class="entity-img" />
+      </figure>
+      <div class="content" style="background-color:transparent;">
+        <p class="title"> </p>
+      </div>
+    </div>
+
+    <div class="swiper-slide">
+      <figure class="slide-bgimga" style="background-image:url(https://i.imgur.com/T6S7tgN.png)">
+        <img src="https://i.imgur.com/T6S7tgN.png" class="entity-img" />
+      </figure>
+      <div class="content" style="background-color:transparent;">
+        <p class="title"> </p>
+      </div>
+    </div>
+
+    <div class="swiper-slide">
+      <figure class="slide-bgimga" style="background-image:url(https://i.imgur.com/T6S7tgN.png)">
+        <img src="https://i.imgur.com/T6S7tgN.png" class="entity-img" />
+      </figure>
+      <div class="content" style="background-color:transparent;">
+        <p class="title"> </p>
+      </div>
+      
+
+     </div>
+   </div>
+</div>
+
+  <script src='https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.0.7/js/swiper.min.js'></script>
+  <script  src="https://wakegg.github.io/assets/js/script1.js?v=144"></script>
+      
+      </body>
+</html>
